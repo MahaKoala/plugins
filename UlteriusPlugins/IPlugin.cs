13fcdf6bdc2f7c64b9984952034d02ff72aea2fa
Andrew <andrew@codeusa523.org>
2016-01-26 16:21:00 -0500

    Add icon/canonical name

--- UlteriusPlugins/IPlugin.cs
+++ UlteriusPlugins/IPlugin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,10 @@ namespace UlteriusPlugins
         string Website { get; }
         string Description { get; }
         Guid GUID { get; }
+        string Icon { get; }
+        string CanonicalName { get; }
         object Start();
         object Start(List<object> args);
+        
     }
 }
