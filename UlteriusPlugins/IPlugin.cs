b513267f1f0bbabd3bb69104a9c3cfb5a0d1f1bc
Andrew <andrew@codeusa523.org>
2016-01-26 05:49:52 -0500

    Plugin interface source code

---
+++ UlteriusPlugins/IPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UlteriusPlugins
+{
+    public interface IPlugin
+    {
+        string Name { get; }
+        double Version { get; }
+        string Author { get; }
+        string Website { get; }
+        string Description { get; }
+        Guid GUID { get; }
+        object Start();
+        object Start(object args);
+    }
+}
